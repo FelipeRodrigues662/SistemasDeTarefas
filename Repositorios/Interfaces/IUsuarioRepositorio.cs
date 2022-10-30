71f66c52948ef7bf0facd266351f002149b2ab64
FelipeRodrigues662 <Felipe.Rod00@hotmail.com>
2022-10-30 17:46:47 -0300

    Adicionar arquivos de projeto.

---
+++ Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using SistemasDeTarefas.Models;
+
+namespace SistemasDeTarefas.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+
+        Task<UsuarioModel> BuscarId(int id);
+
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+
+        Task<bool> Apagar(int id);
+    }
+}
---
+++ Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Data;
+using SistemasDeTarefas.Models;
+using SistemasDeTarefas.Repositorios.Interfaces;
+
+namespace SistemasDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaTarefasDBContex _dBContex;
+
+        public UsuarioRepositorio(SistemaTarefasDBContex sistemaTarefasDBContex)
+        {
+            _dBContex = sistemaTarefasDBContex;
+        }
+
+        public async Task<UsuarioModel> BuscarId(int id)
+        {
+            return await _dBContex.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dBContex.Usuarios.ToListAsync();
+        }
+
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            _dBContex.Usuarios.Add(usuario);
+            _dBContex.SaveChanges();
+
+            return usuario;
+        }
+
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarId(id);
+
+            if(usuarioPorId == null)
+            {
+                throw new Exception($"Usuario para o ID:{id} não foi encontrado no banco de dados.");
+            }
+
+            usuarioPorId.Nome = usuario.Nome;
+            usuarioPorId.Email = usuario.Email;
+
+            _dBContex.Usuarios.Update(usuarioPorId);
+            await _dBContex.SaveChangesAsync();
+
+            return usuarioPorId;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarId(id);
+
+            if (usuarioPorId == null)
+            {
+                throw new Exception($"Usuario para o ID:{id} não foi encontrado no banco de dados.");
+            }
+
+            _dBContex.Usuarios.Remove(usuarioPorId);
+            await _dBContex.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
